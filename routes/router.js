50a65db27f8b6640e43ff04504265986b2119065
naian <530706200@qq.com>
2019-07-25 14:11:11 +0800

    添加404 错误处理

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,18 @@
 const router = require('koa-router')()
 const UserRouter = require('./User')
 
-module.exports = (app) =>{
+module.exports = (app) => {
     router.use('/user', UserRouter.routes(), UserRouter.allowedMethods())
 
     app.use(router.routes()).use(router.allowedMethods())
+    app.use(async (ctx, next) => {
+            switch (ctx.status) {
+                case 404:
+                    ctx.response.body = '404';
+                    break;
+                case 500:
+                    ctx.response.body = '500';
+            }
+        }
+    );
 }
